637322fb35bb973933fcb2ce47e37d6c4bb174ae
deangelign <deangelign@gmail.com>
2016-11-25 11:20:09 -0200

    mip interface

--- Debug/moc_mainwindow.cpp
+++ Debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[30];
-    char stringdata0[805];
+    QByteArrayData data[32];
+    char stringdata0[886];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -58,7 +58,9 @@ QT_MOC_LITERAL(25, 643, 29), // "on_SpinBoxPointZ_valueChanged"
 QT_MOC_LITERAL(26, 673, 30), // "on_SpinBoxVectorX_valueChanged"
 QT_MOC_LITERAL(27, 704, 30), // "on_SpinBoxVectorY_valueChanged"
 QT_MOC_LITERAL(28, 735, 30), // "on_SpinBoxVectorZ_valueChanged"
-QT_MOC_LITERAL(29, 766, 38) // "on_actionGenerate_New_Volume_..."
+QT_MOC_LITERAL(29, 766, 38), // "on_actionGenerate_New_Volume_..."
+QT_MOC_LITERAL(30, 805, 44), // "on_labelFigureXYZ_customConte..."
+QT_MOC_LITERAL(31, 850, 35) // "on_doubleSpinBoxThetaX_valueC..."
 
     },
     "MainWindow\0on_action3D_Image_triggered\0"
@@ -84,7 +86,9 @@ QT_MOC_LITERAL(29, 766, 38) // "on_actionGenerate_New_Volume_..."
     "on_SpinBoxVectorX_valueChanged\0"
     "on_SpinBoxVectorY_valueChanged\0"
     "on_SpinBoxVectorZ_valueChanged\0"
-    "on_actionGenerate_New_Volume_triggered"
+    "on_actionGenerate_New_Volume_triggered\0"
+    "on_labelFigureXYZ_customContextMenuRequested\0"
+    "on_doubleSpinBoxThetaX_valueChanged"
 };
 #undef QT_MOC_LITERAL
 
@@ -94,7 +98,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      26,   14, // methods
+      28,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -102,32 +106,34 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  144,    2, 0x08 /* Private */,
-       3,    1,  145,    2, 0x08 /* Private */,
-       5,    1,  148,    2, 0x08 /* Private */,
-       6,    1,  151,    2, 0x08 /* Private */,
-       7,    0,  154,    2, 0x08 /* Private */,
-       8,    0,  155,    2, 0x08 /* Private */,
-       9,    0,  156,    2, 0x08 /* Private */,
-      10,    0,  157,    2, 0x08 /* Private */,
-      11,    0,  158,    2, 0x08 /* Private */,
-      12,    1,  159,    2, 0x08 /* Private */,
-      14,    1,  162,    2, 0x08 /* Private */,
-      15,    1,  165,    2, 0x08 /* Private */,
-      16,    0,  168,    2, 0x08 /* Private */,
-      17,    0,  169,    2, 0x08 /* Private */,
-      18,    0,  170,    2, 0x08 /* Private */,
-      19,    0,  171,    2, 0x08 /* Private */,
-      20,    0,  172,    2, 0x08 /* Private */,
-      21,    0,  173,    2, 0x08 /* Private */,
-      22,    0,  174,    2, 0x08 /* Private */,
-      23,    1,  175,    2, 0x08 /* Private */,
-      24,    1,  178,    2, 0x08 /* Private */,
-      25,    1,  181,    2, 0x08 /* Private */,
-      26,    1,  184,    2, 0x08 /* Private */,
-      27,    1,  187,    2, 0x08 /* Private */,
-      28,    1,  190,    2, 0x08 /* Private */,
-      29,    0,  193,    2, 0x08 /* Private */,
+       1,    0,  154,    2, 0x08 /* Private */,
+       3,    1,  155,    2, 0x08 /* Private */,
+       5,    1,  158,    2, 0x08 /* Private */,
+       6,    1,  161,    2, 0x08 /* Private */,
+       7,    0,  164,    2, 0x08 /* Private */,
+       8,    0,  165,    2, 0x08 /* Private */,
+       9,    0,  166,    2, 0x08 /* Private */,
+      10,    0,  167,    2, 0x08 /* Private */,
+      11,    0,  168,    2, 0x08 /* Private */,
+      12,    1,  169,    2, 0x08 /* Private */,
+      14,    1,  172,    2, 0x08 /* Private */,
+      15,    1,  175,    2, 0x08 /* Private */,
+      16,    0,  178,    2, 0x08 /* Private */,
+      17,    0,  179,    2, 0x08 /* Private */,
+      18,    0,  180,    2, 0x08 /* Private */,
+      19,    0,  181,    2, 0x08 /* Private */,
+      20,    0,  182,    2, 0x08 /* Private */,
+      21,    0,  183,    2, 0x08 /* Private */,
+      22,    0,  184,    2, 0x08 /* Private */,
+      23,    1,  185,    2, 0x08 /* Private */,
+      24,    1,  188,    2, 0x08 /* Private */,
+      25,    1,  191,    2, 0x08 /* Private */,
+      26,    1,  194,    2, 0x08 /* Private */,
+      27,    1,  197,    2, 0x08 /* Private */,
+      28,    1,  200,    2, 0x08 /* Private */,
+      29,    0,  203,    2, 0x08 /* Private */,
+      30,    1,  204,    2, 0x08 /* Private */,
+      31,    1,  207,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -156,6 +162,8 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::Double,    4,
     QMetaType::Void, QMetaType::Double,    4,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::QPoint,   13,
+    QMetaType::Void, QMetaType::Double,    4,
 
        0        // eod
 };
@@ -192,6 +200,8 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 23: _t->on_SpinBoxVectorY_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
         case 24: _t->on_SpinBoxVectorZ_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
         case 25: _t->on_actionGenerate_New_Volume_triggered(); break;
+        case 26: _t->on_labelFigureXYZ_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 27: _t->on_doubleSpinBoxThetaX_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -222,13 +232,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 26)
+        if (_id < 28)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 26;
+        _id -= 28;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 26)
+        if (_id < 28)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 26;
+        _id -= 28;
     }
     return _id;
 }
